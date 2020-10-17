fccd4720ff86743e347d969084d6f4f2405b56d2
Rocío Mercado <rocio.mercado@astrazeneca.com>
2020-10-17 12:59:16 +0200

    Trying to fix format

--- markdown_generator/talks.py
+++ markdown_generator/talks.py
@@ -92,12 +92,12 @@ for row, item in talks.iterrows():
     md += "---\n"
     
     
-    if len(str(item.event_url)) > 3:
-        md += "\n[event page](" + item.event_url + ")\n" 
-        
     if len(str(item.video_url)) > 3:
         md += "\n[video](" + item.video_url + ")\n" 
     
+    if len(str(item.event_url)) > 3:
+        md += "\n[event page](" + item.event_url + ")\n" 
+        
     if len(str(item.description)) > 3:
         md += "\n" + html_escape(item.description) + "\n"
         
